65358760fdd4d8e2dcf21df7ab3b075e52cc7726
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-09 10:59:24 -0700

    Completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const input = document.querySelector("#new-task-description");
+  const tasks = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // console.log(event.target[0].value);
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "x";
+    li.innerText = input.value + " ";
+    tasks.appendChild(li).appendChild(btn);
+    event.target.reset();
+    btn.addEventListener("click", function () {
+      tasks.removeChild(li);
+    });
+  });
 });
